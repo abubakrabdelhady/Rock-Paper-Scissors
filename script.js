2f0cdc032358ce9a0e2568ebd6f42cede10de6d7
Abubakr Abdelhady <171250585+abubakrabdelhady@users.noreply.github.com>
2024-06-06 18:16:06 +0300

    Get computer and human choices

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log(`Let's Play Rock Paper Scissors!`);
+
+function getComputerChoice() {
+    // get random choice from “rock”, “paper” or “scissors”.
+    let choices = ['rock', 'paper', 'scissors'];
+    let choice = Math.floor(Math.random() * choices.length);
+    return choices[choice];
+}
+
+let computerChoice = getComputerChoice();
+
+let humanChoice = prompt(`Enter your choice (Rock or Paper or Scissors)`);
+
+console.log(computerChoice, humanChoice);
